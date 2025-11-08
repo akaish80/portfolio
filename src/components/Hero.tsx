8e5a75512fa8de39ef0a2eaa057a8ab398419fbb
Arunkumar Dharmarajan <akaish@gmail.com>
2025-11-07 23:32:27 -0500

    fix: build

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,10 +6,10 @@ const Hero: React.FC = () => {
     <section id="hero" className="hero d-flex ">
       <div className="container">
         <div className="row gy-4 justify-content-center">
-          <div className="col-lg-6 order-1 order-lg-2 hero-img">
+          <div className="col-lg-6 order-1 order-lg-1 hero-img">
             <img src="img/hero-developer.svg" className="img-fluid" alt="" />
           </div> 
-          <div className="order-2 order-lg-1 d-flex flex-column text-center" data-aos="zoom-in" data-aos-delay="100">
+          <div className="order-2 order-lg-2 d-flex flex-column text-center" data-aos="zoom-in" data-aos-delay="100">
             <h2>Arun Kumar</h2>
             <h3>I'm a Lead front end developer, specialized in development of web applications using React, Redux</h3>
             <div>
